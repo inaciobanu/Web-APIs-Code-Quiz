7f543aad7ca2077412e8034c71b54317d39d40fb
Ina <cati_ciobanu@yahoo.com>
2023-01-13 19:48:04 +0000

    created 2 js files one for questions one for logic and added THE questions

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,56 @@
+{
+    question: "If you type the following code in the console window, what result will you get?
+    3 > 2 > 1 === false;",
+    answers: [
+        "true",
+        "false",
+        "undefined"
+    ],
+    correctAnswerIndex: 1
+},
+{
+    question: "JavaScript File Has An Extension of:",
+    answers: [
+        ".java",
+        ".js",
+        ".xml",
+        ".javascript"
+    ],
+    correctAnswerIndex: 2
+},
+{
+    question: "Which Of The Dialog Box Display a Message And a Data Entry Field?",
+    answers: [
+        "Msg()",
+        "Alert()",
+        "Prompt()",
+        "Confirm()"
+    ],
+    correctAnswerIndex: 3
+},
+
+{
+    question: "A Function Associated With An object is Called:",
+    answers: [
+        "Function",
+        "Hyperlink",
+        "Method",
+        "Functional Atribute"
+    ],
+    correctAnswerIndex: 4
+},
+
+{
+    question: "Which command creates a new button?",
+    answers: [
+        "let button = document.new('button');",
+        "let button = document.createElement('');",
+        "let button = document.createElement('button');", "CORRECT"
+        "let button = document.makeElement('{button}');"
+    ],
+    correctAnswerIndex: 5
+},
+
+
+
+
